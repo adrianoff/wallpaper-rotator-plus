d8b160e15255b3a692b5a07c58463f141849ef6c
adrianoff <adrianov87@gmail.com>
2020-04-04 01:36:39 +0300

    wrp-server source

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # wallpaper-rotator-plus
 
 Desktop App for creating and setup wallpaper from any source like fotki.yandex.ru
+
+
+How to create virtual env: 
+
+        virtualenv -p /usr/bin/python3 venv/
+        
+        source venv/bin/activate
+        
+        pip install -r requirements.txt
--- Source/AdrianovProSource.py
+++ Source/AdrianovProSource.py
@@ -1,4 +1,6 @@
 from Source.AbstractSource import AbstractSource
+import requests
+import json
 
 
 class AdrianovProSource(AbstractSource):
@@ -6,10 +8,10 @@ class AdrianovProSource(AbstractSource):
         super().__init__(wallpapers_dir)
 
     def get_image_info(self):
-        return {
-            'url': 'https://upload.wikimedia.org/wikipedia/commons/c/c9/Avtoportret_Gelyi_Korzev.jpg',
-            'author': 'Гелий Коржев',
-            'title': 'Название картины',
-            'year': '1961',
-            'info_link': 'https://ru.wikipedia.org/wiki/%D0%9A%D0%BE%D1%80%D0%B6%D0%B5%D0%B2,_%D0%93%D0%B5%D0%BB%D0%B8%D0%B9_%D0%9C%D0%B8%D1%85%D0%B0%D0%B9%D0%BB%D0%BE%D0%B2%D0%B8%D1%87'
-        }
+        url = 'http://localhost:8000/api/random/picture'
+        req = requests.get(url, verify=False)
+        result = json.loads(req.text)
+        result['url'] = 'http://localhost:8000/' + result['file']
+        result['info_link'] = result['link_info']
+
+        return result
